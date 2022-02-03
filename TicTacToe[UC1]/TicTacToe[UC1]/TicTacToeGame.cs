38fbbbaebc20e36e3448dc1c29540da839cdc9b9
Apara1330 <polapara1330@gmail.com>
2022-02-03 10:10:56 +0530

    [Apara]- TicTacToe Game UC1 completed

---
+++ TicTacToe[UC1]/TicTacToe[UC1]/TicTacToeGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_UC1_
+{
+     class TicTacToeGame
+    {
+        static char[] arr = { '0','1','2','3','4','5','6','7','8','9'};
+        static int flag =0;
+        static int player = 1;
+    }
+}
